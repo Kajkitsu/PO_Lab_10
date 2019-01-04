f0615117bfc223449f6d296c85c76f188c48b135
Zdunke <zdun.piotr@gmail.com>
2019-01-04 09:45:12 +0100

    ManagerFaktur

---
+++ ManagerFaktur.java
@@ -0,0 +1,25 @@
+import java.util.List;
+public class ManagerFaktur{
+    private List<Faktura> faktury;
+    public ManagerFaktur(){
+
+    }
+    public void utworzFakture(String numer){
+        
+    }
+    public Faktura znajdzFakture(String numer){
+
+    }
+    public List<Faktura> znajdzFakture(Produkt produkt){
+
+    }
+    public List<Faktura> znajdzFakture(double cenaFrom, double cenaTo){
+
+    }
+    public void usunFakture(String numer){
+
+    }
+    public void wyswietlNazwyFaktur(){
+        
+    }
+}
